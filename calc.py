c34252668eeaa22f7fe31fc4580e5380394705ba
Christian Boyer <chboyer1995@yahoo.com>
2017-11-11 16:40:19 -0600

    test out improved calculation

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 def wind_speed(u, v, w):
     """Calculates wind speed from u, v, and w components."""
     w = 0
-    return sqrt(u * u + v * v + w * w)
+    return (u * u + v * v + w * w)
 
 def calc():
     return 5
